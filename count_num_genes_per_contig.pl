786a5a60eedf692f9fcd7c0d77159204f66f046c
Philip MacMenamin <philipmac@rutgers.edu>
2013-06-12 12:20:40 -0400

    look at the gene calls, and output a summary of what genes are extant in each strain, in each contigset

---
+++ count_num_genes_per_contig.pl
@@ -0,0 +1,35 @@
+use warnings;
+use strict;
+
+my @files = `ls ../ecolireads/*_31/blat/gene_calls/Escherichia_coli*.csv`;
+system "rm gene_counts/*";	# important, I am catting output
+
+foreach my $file (@files){
+
+    # prep
+    chomp $file;
+    my (undef, undef, $contigSet,undef, undef, $strain)=split /\//,$file;
+    $strain =~ s/\.csv//;
+
+    open IN, $file or die $!;
+    my %p_to_gene;
+    while (<IN>){
+	chomp;
+	my ($p,$gene)=split ',';
+	push @{$p_to_gene{$p}},$gene;
+    }
+    close IN;
+    
+    # loop through the plasmids, sum up the gene content of each
+    my $plasmids = '';
+    my $totNumGenes=0;
+    foreach (keys %p_to_gene){
+	$totNumGenes += scalar @{$p_to_gene{$_}};
+	$plasmids .= $_.' ('.scalar @{$p_to_gene{$_}}.") ";
+    }
+
+    # note catting output
+    open OUT, ">>gene_counts/$contigSet".'_gene_counts';
+    print OUT join "\t", ($totNumGenes,$strain,$plasmids,"\n");
+    close OUT;
+}
