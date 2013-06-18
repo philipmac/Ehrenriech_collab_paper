dc4ac1427d23e1251b29ec23d100be75d23462cc
Philip MacMenamin <philipmac@rutgers.edu>
2013-06-18 10:53:22 -0400

    script with creates a dir containing the best/top gene list for every contig set

---
+++ get_best_strain_genes_per_sample.pl
@@ -0,0 +1,19 @@
+# just wanted to assemble all of the best species picks into one place...
+
+use strict;
+use warnings;
+
+open (IN, "top_picks_merged_with_Ian_work") or die $!;
+
+while (<IN>){
+
+    #barcode E_coli_strain_called num_genes_common type host
+    #AATGGC Escherichia_coli_CFT073_uid57915 3259 5 4
+    chomp;
+    next if $_ =~ /barcode/i;
+    my ($sample,$strain,  undef) = split /\t/,$_;
+    $sample .='_31';
+    system "cp ../ecolireads/$sample/blat/gene_calls/$strain.csv best_strain/$sample.$strain";
+
+}
+close IN;
