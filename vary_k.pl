887b49c7c2434df99c5406930a81a7cc61c6d164
Philip MacMenamin <philipmac@rutgers.edu>
2013-06-23 22:45:38 -0400

    applying this script to low covg data

--- vary_k.pl
+++ vary_k.pl
@@ -1,21 +1,21 @@
 use strict;
 use warnings;
 
+my @files = `ls ../scratch/*.fa`;
 
-my @files = `ls ../L37/*per*fa`;
 foreach my $file (@files){
     chomp $file;
     
-    my @f = split /\//,$file;
+    #my @f = split /\//,$file;
 
     foreach my $k (29,31,33,35,37){
-	my $le = $f[2];
-	$le =~ s/\.fa/_$k/;
-	my $outDir = join '/',($f[0],$f[1],$le);
-    
-	print $outDir,"\n";
-	system "/home/philip/code/velvet/velveth $outDir $k -shortPaired $file";
-	system "/home/philip/code/velvet/velvetg $outDir -ins_length 300 -cov_cutoff 2 -exp_cov 40";
+
+	my $outDir = $file;
+	$outDir =~ s/\.fa/_k_$k/;
+	system "mkdir $outDir" unless (-d $outDir);
+
+	system "~/packages/velvet_1.2.10/velveth $outDir $k -shortPaired $file";
+	system "~/packages/velvet_1.2.10/velvetg $outDir -ins_length 300 -cov_cutoff 2 -exp_cov 40";
 
     }
 
