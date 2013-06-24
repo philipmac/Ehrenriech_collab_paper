ec1c36f095f5e3f86fd394873e7124b19887bdc6
Philip MacMenamin <philipmac@rutgers.edu>
2013-06-23 22:45:01 -0400

    updateing this to apply it to the low covg data

--- vary_covg.pl
+++ vary_covg.pl
@@ -1,17 +1,18 @@
 use strict;
 use warnings;
 
-my @files = `ls ../L37/*/contigs.fa`;
+my $base = '../scratch/AAAAGG_k_31';
 
-foreach my $file (@files){
-    chomp $file;
-    my $outDir = $file;
-    $outDir =~ s/contigs\.fa//;
+my $max_covg = 100;
+my $covg = 20;
+my $step = 10;
 
-
-    print $file;
-    foreach my $covg (100){
-    	system "/home/philip/code/velvet/velvetg $outDir -ins_length 300 -cov_cutoff 2 -exp_cov $covg";
-    }
-    last;
+while ($covg <= $max_covg){
+    my $outDir = $base."_asmbl_$covg";
+    #print $outDir,"\n";
+    system "mkdir $outDir" unless (-d $outDir);
+    system "cp $base/Roadmaps $outDir/Roadmaps";
+    system "cp $base/Sequences $outDir/Sequences";
+    system "/home/philip/packages/velvet_1.2.10/velvetg $outDir -ins_length 300 -cov_cutoff 2 -exp_cov $covg";
+    $covg += $step;
 }
