edde638af89e3f387ec0f0b0e440e3eacd67583c
Philip MacMenamin <philipmac@rutgers.edu>
2013-06-28 13:54:51 -0400

    time point 2. This is the "normal" ie low coverage data.

---
+++ tp2/vary_k.pl
@@ -0,0 +1,18 @@
+# this is not in use at the moment.
+use strict;
+use warnings;
+
+my @samples = `ls ../reads/*/merged.fa`;
+my $covg = 30;
+my $k = 31;
+my $repl = 'k_'.$k.'_covg_'.$covg;
+foreach my $sample (@samples){
+    chomp $sample;
+
+    my $outDir = $sample;
+    $outDir =~ s/merged\.fa/$repl/;
+    system "mkdir $outDir" unless (-d $outDir);
+
+    system "~/packages/velvet_1.2.10/velveth $outDir $k -shortPaired $sample";
+
+}
