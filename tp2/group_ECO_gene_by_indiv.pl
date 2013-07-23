f0f02103cb8b37d94f3827dcc79f7bc17f5631d1
Philip MacMenamin <philipmac@rutgers.edu>
2013-07-23 15:29:13 -0400

    cleanup

--- tp2/group_ECO_gene_by_indiv.pl
+++ tp2/group_ECO_gene_by_indiv.pl
@@ -1,41 +1,16 @@
+# this makes :
+# step 1) want to make sets like :
+# tp2.barcode1 {a,b,c} 	  (indiv 1)
+# tp2.barcode2 {a,b,c}	  (indiv 1)
+# tp2.barcode3 {a,b,e}	  (indiv 1)
+# tp2.barcode3 {b,c,d}	  (indiv 2)
+# tp2.barcode3 {b,c,e}	  (indiv 2)
+# tp2.barcode3 {b,c,f}	  (indiv 3)
+
 use strict;
 use warnings;
 
-my %barcode_to_host;
-
-open IN, 'derived_data/barcodes_tp2.txt' or die $!;
-while (<IN>){
-    # F=>1; I=>2, J=>3, S=>4 L=>5.
-    chomp $_;
-    next if $_ =~ /exclude/;
-    my ($bc,$well,undef) = split /\t/,$_;
-
-    if ($well =~ /^F/){
-        $barcode_to_host{$bc}='1';
-    }
-    elsif ($well =~ /^I/){
-        $barcode_to_host{$bc}='2';
-    }
-    elsif ($well =~ /^J/){
-        $barcode_to_host{$bc}='3';
-    }
-    elsif ($well =~ /^S/){
-        $barcode_to_host{$bc}='4';
-    }
-    elsif ($well =~ /^L/){
-        $barcode_to_host{$bc}='5';
-    }
-    else{
-        die "print Borking on $_\n";
-    }
-}
-close IN;
-
-# open up all the eck gene lists, 
-# sort them into which host they are in
-# print hosts
-
-my %host_to_ECO_genes;
+my %barcode_to_ECO_genes;
 
 my @files = `ls ~/e_reich/tp2/reads/*/k_31_covg_30/blat/*.ECO_GENES_hit`;
 foreach my $file (@files){
@@ -46,20 +21,15 @@ foreach my $file (@files){
 
     next if $barcode =~ /CGAAAT/;
 
-    my $host = $barcode_to_host{$barcode};
-    print $file,"\n" if $host eq '';
     while (<IN>){
 	chomp $_;
-	$host_to_ECO_genes{$host}{$_}++
+	$barcode_to_ECO_genes{$barcode}{$_}=1;
     }
     close IN;
 }
 
-
-foreach my $host (keys %host_to_ECO_genes){
-    open OUT, ">derived_data/ECO_GENE_tp1_HOST.$host";
-    foreach (sort {$host_to_ECO_genes{$host}->{$b}<=>$host_to_ECO_genes{$host}->{$a}} keys %{$host_to_ECO_genes{$host}}){
-	print OUT $_,"\t",$host_to_ECO_genes{$host}->{$_},"\n";
-    }
+foreach my $barcode (keys %barcode_to_ECO_genes){
+    open OUT, ">derived_data/ECO_GENE_tp2_HOST.$barcode";
+    print OUT join "\n",keys %{$barcode_to_ECO_genes{$barcode}};
     close OUT;
 }
