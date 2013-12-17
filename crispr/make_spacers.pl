345f3e4b90b31ed71b4b9933b22c9e38b165bba3
Philip MacMenamin <philipmac@rutgers.edu>
2013-12-17 17:19:00 -0500

    some small refactoring and integ with Eco.pm

--- crispr/make_spacers.pl
+++ crispr/make_spacers.pl
@@ -3,42 +3,116 @@
 
 use strict;
 use warnings;
+use Eco;
 
-system "rm /home/philip/e_reich/tp1_hi_covg/reads/*/*_ds_4k_31_covg_30/bowtie_index/spacers.fa";
-system "rm /home/philip/e_reich/tp1_hi_covg/reads/*/*k_57_covg_100/bowtie_index/spacers.fa";
-system "rm /home/philip/e_reich/tp1_454/*bowtie_index/spacers.fa";
-system "rm /home/philip/e_reich/tp2/reads/*/k_31_covg_30/bowtie_index/spacers.fa";
-system "rm /home/philip/e_reich/tp1/reads/*_31/bowtie_index/spacers.fa";
-
-my @repeatFiles2 = `ls /home/philip/e_reich/tp2/reads/*/k_31_covg_30/bowtie_index/rep_locs.out`;
-my @repeatFiles1 = `ls /home/philip/e_reich/tp1/reads/*/bowtie_index/rep_locs.out`;
-my @reps_454 = `ls /home/philip/e_reich/tp1_454/*bowtie_index/rep_locs.out`;
-my @reps1_hi_covg_ds = `ls /home/philip/e_reich/tp1_hi_covg/reads/*/*_ds_4k_31_covg_30/bowtie_index/rep_locs.out`;
-my @reps1_hi_covg = `ls /home/philip/e_reich/tp1_hi_covg/reads/*/*_57_covg_100/bowtie_index/rep_locs.out`;
-
-my @reps = (@reps_454, @reps1_hi_covg_ds, @reps1_hi_covg, @repeatFiles1 , @repeatFiles2);
-my %ctg_to_repeats = %{load_repeats(\@reps)}; # # @repeatFiles1,
-
-my @contigs1 =  `ls /home/philip/e_reich/tp1/reads/*_31/contigs.fa`;
-my @contigs2 = `ls /home/philip/e_reich/tp2/reads/*/k_31_covg_30/contigs.fa`;
-my @contigs_454 = `ls /home/philip/e_reich/tp1_454/*.fasta`; # not really contigs
-my @contigs_hi_covg_ds = `ls /home/philip/e_reich/tp1_hi_covg/reads/*/*_ds_4k_31_covg_30/contigs.fa`; # down sampled...
-my @contigs_hi_covg = `ls /home/philip/e_reich/tp1_hi_covg/reads/*/*_57_covg_100/contigs.fa`;
-
-my @conts = (@contigs_454,@contigs_hi_covg_ds, @contigs_hi_covg, @contigs2, @contigs1 );
-my %contig_seqs = %{load_contig_seqs(\@conts)};
-
-my %spcrs;
-
-my $too_long = 43;
+# http://www.sciencemag.org/content/327/5962/167.full.html
+my $too_long = 72;
 my $too_short = 20;
 
 open TOO_SHORT, ">too_short";
 open TOO_LONG, ">too_long";
 
+clear_out_spacers();		# nuke existing spacers.
+
+# these are the files where the repeat locations are found per isolate;
+# mapping name to file location.
+my %tp1_tp2_rep_locs = %{list_tp1_tp2_repeats()};
+my %hi_cov_454_rep_locs = %{list_hi_covg_and_454_repeats()};
+
+foreach my $isolate (keys %tp1_tp2_rep_locs){
+    my %annots = %{load_repeat_loc_annotation($tp1_tp2_rep_locs{$isolate})};
+
+    # grab the relevant contigs    
+    my %contigs = %{load_specific_contigs(\%annots)};
+
+    write_spacers(\%annots,\%contigs);
+    # do small check making sure that the spacer seems sane.
+}
+
+foreach my $isolate (keys %hi_cov_454_rep_locs){
+    my %annots = %{load_repeat_loc_annotation($hi_cov_454_rep_locs{$isolate})};
+
+    # grab the relevant contigs    
+    my %contigs = %{load_specific_contigs(\%annots)};
+
+    write_spacers(\%annots,\%contigs);
+}
+    
+close OUT;
+close TOO_LONG;
+
+my %ctg_to_repeats; #; = %{load_repeats(\@all_rep_locs)};
+
+exit;
+
+sub write_spacers{
+
+    my %repeat_annots = %{$_[0]};
+    my %contigs = %{$_[1]};
+
+    foreach my $ctg_name (keys %repeat_annots){
+	my $ctg_seq = $contigs{$ctg_name};
+
+	next if $ctg_name =~ /BC|TP/;
+	
+	my @reps = @{$repeat_annots{$ctg_name}};
+
+	my $spc_start=0;
+	my $spcr_num=0;
+	my $prev_rep ='';
+	foreach my $repeat (sort { $a->{LOC} <=>$b->{LOC} } @{reps} ){
+#	next unless $repeat->{STRAND} eq '-';
+	    my $rep_end = $repeat->{LOC}+length($repeat->{SEQ});
+	    next if $repeat->{OLAPS} && !$repeat->{DOM};
+	    next unless $rep_end;
+
+	    my $comp_rep = '';
+	    if ($spc_start){
+
+		$spcr_num++;
+
+		my $spc_len = $repeat->{LOC} - $spc_start; # ie this rep start - last rep start 
+		my $spacer = substr $ctg_seq, $spc_start, $spc_len;
+
+		next if $spacer =~ /N/;
+		#$spacer =~ s/N//g;
+
+		die "no rep seq \n" if !defined $repeat->{SEQ};
+		die "no rep loc \n" if !defined $repeat->{LOC};
+
+		$comp_rep = substr $ctg_seq, $repeat->{LOC}, length($repeat->{SEQ}); # this is just to see the computed repeat
+
+		if (length($spacer) > $too_long){
+		    print TOO_LONG join "\t",(">$ctg_name","spacer:$spcr_num", $prev_rep,$comp_rep, "rep:".$repeat->{SEQ}.'<',"\n");		
+		    print TOO_LONG $spacer,"\n";
+		    print TOO_LONG "\n___________ end ___________\n";
+
+		}
+		elsif (length($spacer) < $too_short){
+		    print TOO_SHORT join "\t",(">$ctg_name","spacer:$spcr_num", $prev_rep,$comp_rep, "rep:".$repeat->{SEQ}.'<',"\n");		
+		    print TOO_SHORT $spacer,"\n";
+		    print TOO_SHORT "\n___________ end ___________\n";
+		}		
+		else{
+		    my $file= $repeat->{FILE};
+		    $file =~ s/rep_locs\.out/spacers\.fa/;
+		    die "nope" if $file eq $repeat->{FILE};
+
+		    open OUT, ">>$file" or die $!;
+		    print OUT join "\t",(">$ctg_name","spacer:$spcr_num",$repeat->{'TP'},"\n"); #  $prev_rep,$comp_rep, 
+		    print OUT $spacer,"\n";
+		    close OUT;
+		}
+	    }
+	    $spc_start=$rep_end;
+	    $prev_rep=$comp_rep;
+	}
+    }
+}
+
 foreach my $ctg_name (keys %ctg_to_repeats){
-    die "no ctg for $ctg_name\n" if !defined $contig_seqs{$ctg_name};
-    my $ctg_seq = $contig_seqs{$ctg_name};
+    # die "no ctg for $ctg_name\n" if !defined $contig_seqs{$ctg_name};
+    my $ctg_seq;# = $contig_seqs{$ctg_name};
     
     my @reps = @{$ctg_to_repeats{$ctg_name}};	    
 
@@ -94,76 +168,9 @@ foreach my $ctg_name (keys %ctg_to_repeats){
 	$prev_rep=$comp_rep;
     }
 }
-close OUT;
-close TOO_LONG;
 
 
 
-sub load_contig_seqs{
-
-    my %h;
-    foreach my $file (@{$_[0]}){
-	my $need=0;
-
-	open IN, $file or die $!;
-	while (<IN>){
-	    chomp;
-	    next if $_ =~ /^\s*$/;
-	    if ($_ =~ /^>(\w+\.?\w*)/){	    
-
-#		print $_,"\n",$1,"\n";
-
-		if ($ctg_to_repeats{$1}){
-		    $need = $1;
-		}
-		else{
-		    $need = 0
-		}
-	    }
-	    elsif($need){
-		$h{$need} .=$_;
-	    }
-	}
-	close IN;
-    }
-    return \%h;
-}
-
-
-sub load_repeats{
-    my %reps;
-    foreach my $file (@{$_[0]}){
-	chomp $file;
-	my $bc = $1 if $file =~ /reads\/(\w+)\//;
-	$bc = $1 if $file =~ /tp1_454\/(\w+)bowtie_index/;
-
-	open IN, $file or die $!;
-
-	while (<IN>){
-	    # cointig name                      offset seq                        olap? tie breaker! strand timepoint
-	    # NODE_9909_length_60_cov_48.633335 0  CGGTTTATCCCCGCTGACGCGGGGAACAC   0   0  + tp1_454
-	    # NODE_9909_length_60_cov_48.633335 1  GGTTTATCCCCGCTGGCGCGGGGAACAC    0   1  + tp1_454
-	    chomp;
-	    my ($ctg_name,$loc,$seq,$is_olap,$tb,$strand,$tp) = split /\t/,$_;
-
-	    # print $file,":",$_,"\n" if $ctg_name eq 'GCGGTTTATCCCCGCTGGCGCGGGGAACTC'; #name eq 'NODE_741_length_58_cov_9.758620';
-	    my %r;
-	    $r{FILE}=$file;
-	    $r{TP}=$tp;
-	    $r{BC}=$bc;
-	    $r{SEQ}=$seq;
-	    $r{OLAPS}=$is_olap;
-	    $r{DOM}=$tb;
-	    $r{STRAND}=$strand;
-	    $r{LOC}=$loc;
-	    push @{$reps{$ctg_name}},\%r;
-	}
-
-	close IN or warn;	    
-    }
-
-    return \%reps;
-}
 
 
 
