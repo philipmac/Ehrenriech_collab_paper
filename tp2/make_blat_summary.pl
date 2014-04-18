0e0a7d875b8638231e270d5250240e8d9e4fb32f
Philip MacMenamin <philipmac@rutgers.edu>
2014-04-17 20:00:44 -0400

    bug fix & moving code to ECO.pm

--- tp2/make_blat_summary.pl
+++ tp2/make_blat_summary.pl
@@ -1,47 +1,9 @@
 use warnings;
 use strict;
-
-# runs through the psl output of blat, and prunes off the stuff we're not interested in. 
-# outputs a csv file.
-
+use Eco;
 
 my @psl_files = `ls ../../tp2/reads/*/k_31_covg_30/blat/*.psl`;
 
-foreach my $file (@psl_files){
-    chomp $file;
-
-    open IN, $file or die $!;
-
-    my $outFile = $file;
-    $outFile =~ s/\.psl/\.csv/;
-    open OUT, ">$outFile" or die $!;
-
-    while (<IN>){
-    	next unless $_ =~ /^\d/;
-    	my @a = split /\t/,$_;
-	my $faHeader = $a[13];
-	my @b = split /\|/,$faHeader;
-	my $nc_name = $b[$#b-1];
-	my $loc = $b[$#b];
-	$loc =~ s/://;
-
-	my $geneLen=$a[10];
-	foreach ($a[$#a-2],$a[$#a-1]){ # rms trailing commas from some fields where we dont want them
- 	    $_=~ s/,$//;
-	}
-	my @blockSizes = split /,/,$a[$#a-2];
-	my $lens = join ',',@blockSizes;
-
-	my @starts = split /,/,$a[$#a-1];
-	my $startAsString = join ',',@starts;
-	my $strand = $a[8];
-	print OUT join "\t",($nc_name,$loc,$geneLen,$lens,$startAsString,$strand);
-	print OUT "\n";
-	
-    }
-    close IN;
-    close OUT;
-
-}
+strip_blat_cols(\@psl_files);
 
 print "blat parse done...\n";
